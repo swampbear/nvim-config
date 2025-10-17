d8c5b3a07825d1d76eeb1bb409f13d4b4b6753ed
Jobjorn <672641@stud.hvl.no>
2025-10-17 14:55:59 +0200

    changed alpha logo to swampvim

--- lua/jobjorn/plugins/alpha.lua
+++ lua/jobjorn/plugins/alpha.lua
@@ -1,40 +1,43 @@
-
 -- alpha-config.lua
 
-local status_ok, alpha = pcall(require, "alpha")
+local status_ok, alpha = pcall(require, 'alpha')
 if not status_ok then
- return
+	return
 end
+vim.api.nvim_set_hl(0, 'AlphaHeader', {
+	fg = '#A597BA', -- This is a nice blue color (hex code)
+	bold = true, -- Make it bold for extra impact
+})
 
-local dashboard = require("alpha.themes.dashboard")
+local dashboard = require 'alpha.themes.dashboard'
+
 dashboard.section.header.val = {
-    [[███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
-      [[████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
-      [[██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
-      [[██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
-      [[██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
-      [[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-                                                        
+	[[███████╗██╗    ██╗ █████╗ ███╗   ███╗██████╗ ██╗   ██╗██╗███╗   ███╗]],
+	[[██╔════╝██║    ██║██╔══██╗████╗ ████║██╔══██╗██║   ██║██║████╗ ████║]],
+	[[███████╗██║ █╗ ██║███████║██╔████╔██║██████╔╝██║   ██║██║██╔████╔██║]],
+	[[╚════██║██║███╗██║██╔══██║██║╚██╔╝██║██╔═══╝ ╚██╗ ██╔╝██║██║╚██╔╝██║]],
+	[[███████║╚███╔███╔╝██║  ██║██║ ╚═╝ ██║██║      ╚████╔╝ ██║██║ ╚═╝ ██║]],
+	[[╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝       ╚═══╝  ╚═╝╚═╝     ╚═╝]],
 }
 
- dashboard.section.buttons.val = {
-   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-   dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-   dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
-   dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+dashboard.section.buttons.val = {
+	dashboard.button('f', '  Find file', ':Telescope find_files <CR>'),
+	dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
+	dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
+	dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
+	dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.vim<CR>'),
+	dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
 }
 
 local function footer()
- return "Don't Stop Until You are Proud..."
+	return "Don't Stop Until You are Proud..."
 end
 
 dashboard.section.footer.val = footer()
 
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
+dashboard.section.footer.opts.hl = 'Type'
+dashboard.section.header.opts.hl = 'AlphaHeader'
+dashboard.section.buttons.opts.hl = 'Keyword'
 
 dashboard.opts.opts.noautocmd = true
 alpha.setup(dashboard.opts)
