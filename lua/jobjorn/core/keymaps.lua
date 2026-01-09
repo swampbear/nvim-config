f9e9626aab3cad2f5787968e0c0e6ea9229eb7a6
Jobjorn <672641@stud.hvl.no>
2026-01-09 08:03:53 +0100

    feat: add opencode keymaps

--- lua/jobjorn/core/keymaps.lua
+++ lua/jobjorn/core/keymaps.lua
@@ -26,3 +26,32 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Recommended/example keymaps.
+vim.keymap.set({ 'n', 'x' }, '<leader>ai', function()
+	require('opencode').ask('@this: ', { submit = true })
+end, { desc = 'Ask opencode' })
+vim.keymap.set({ 'n', 'x' }, '<leader>x', function()
+	require('opencode').select()
+end, { desc = 'Execute opencode action…' })
+vim.keymap.set({ 'n', 't' }, '<leader>.', function()
+	require('opencode').toggle()
+end, { desc = 'Toggle opencode' })
+
+vim.keymap.set({ 'n', 'x' }, 'go', function()
+	return require('opencode').operator '@this '
+end, { expr = true, desc = 'Add range to opencode' })
+vim.keymap.set('n', 'goo', function()
+	return require('opencode').operator '@this ' .. '_'
+end, { expr = true, desc = 'Add line to opencode' })
+
+vim.keymap.set('n', '<S-C-u>', function()
+	require('opencode').command 'session.half.page.up'
+end, { desc = 'opencode half page up' })
+vim.keymap.set('n', '<S-C-d>', function()
+	require('opencode').command 'session.half.page.down'
+end, { desc = 'opencode half page down' })
+
+-- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
+vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment', noremap = true })
+vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement', noremap = true })
