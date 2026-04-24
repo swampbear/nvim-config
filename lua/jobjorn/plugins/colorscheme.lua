d28545e918697c32b8731f7179ae6886a07b0912
swampbear <jobjorn.myren@gmail.com>
2026-04-24 10:22:22 +0200

    set colorscheme to default

--- lua/jobjorn/plugins/colorscheme.lua
+++ lua/jobjorn/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-    {
-        'catppuccin/nvim',
-        name = 'catppuccin',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require('catppuccin').setup {
-                flavour = 'macchiato',
-            }
-            -- Set default colorscheme (change to name to enable different themes)
-            vim.cmd.colorscheme 'catppuccin'
-        end,
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato",
+			})
+			-- Set default colorscheme (change to name to enable different themes)
+			vim.cmd.colorscheme("default")
+		end,
+	},
 }
